9c38091ab5d5e19c0fecfeb263a662d225198c46
powerumc <powerumc@gmail.com>
2025-07-17 22:40:32 +0900

    chore: remove clang, csharp examples

--- examples/nodejs/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "# Before hostshook is loaded"
-node testdns.js
-echo
-
-echo "# Hostshook is loaded"
-export DYLD_INSERT_LIBRARIES=$(pwd)/../../target/debug/libhostshook.dylib
-node testdns.js
